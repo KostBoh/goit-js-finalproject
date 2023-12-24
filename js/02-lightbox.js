64bf9e6cdc3d0c4874fcad92baad1b2ca280a313
KostBoh <bohdan.kostya@gmail.com>
2023-12-24 19:35:57 +0200

    add begin task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+function createMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }, index) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"/>
+            </a>
+        </li>`
+    )
+    .join("");
+}
+
+galleryList.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+galleryList.addEventListener("click", handlerGaleryClick);
+
+function handlerGaleryClick(evt) {
+  evt.preventDefault();
+  if (evt.currentTarget === evt.target) {
+    return;
+  }
+  const clickedImageSrc = evt.target.getAttribute("data-source");
+  const clickedImageObject = galleryItems.find(
+    (item) => item.original === clickedImageSrc
+  );
+
+  console.log(clickedImageObject);
+}
